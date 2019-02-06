676739216ad64faf56746fd61f321946be28672d
Alyonkalis <Alyonka_lis@mail.ru>
2019-02-06 16:04:35 +0300

    search

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var buttonSearch = document.querySelector('.user-navigation__item--search'),
+	menuSearch = document.querySelector('.search');
+
+buttonSearch.addEventListener("click", function(){                //слушаем клик по кнопке
+    if(menuSearch.classList.contains('open')){ 
+        menuSearch.classList.remove('open');                      //удаляем класс overflow у body
+    } else {                                                     //если нет класса open
+        menuSearch.classList.add('open');                         //добавляем класс overflow для body
+    }
+});

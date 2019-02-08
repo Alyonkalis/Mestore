4dcb1802ae90ca6b8dfc15b041a95bced69d688f
Alyonkalis <Alyonka_lis@mail.ru>
2019-02-08 16:20:37 +0300

    ready

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-var buttonSearch = document.querySelector('.fa-search'),
-	menuSearch = document.querySelector('.search');
+var buttonSearch = document.querySelector('.fa-search'), 
+	menuSearch = document.querySelector('.search'); 
 
-buttonSearch.addEventListener("click", function(){                //слушаем клик по кнопке
-    if(menuSearch.classList.contains('open')){ 
-        menuSearch.classList.remove('open');                      //удаляем класс overflow у body
-    } else {                                                     //если нет класса open
-        menuSearch.classList.add('open'),
-        menuSearch.querySelector('input').focus();               //фокус инпуту в поиске
-    }
-});
+document.addEventListener("click", function(event) { 
+	if(event.target === buttonSearch){ //проверяем, является ли элемент, по которому мы кликнули кнопкой поиска
+		menuSearch.classList.toggle('open'); //Если да => переключаем класс open и добавляем фокус инпуту,
+		menuSearch.querySelector('input').focus(); 
+	} else { 
+		if(!event.target.classList.contains('searchInput')){    //проверяем не кликнули ли мы на поле поиска,
+		menuSearch.classList.remove('open'); 					//если это не оно, то убираем класс open
+	} 
+} 
+})
